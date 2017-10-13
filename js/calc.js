c87329257d87f94bc2a7b2cd42b6112c40cf1904
Rafael Furry <rfurry@gmail.com>
2017-10-13 13:13:29 -0700

    create earthToMercury

--- js/calc.js
+++ js/calc.js
@@ -6,4 +6,27 @@ export class Calc {
   yearsToSeconds() {
     return (this.age * 31536000);
   }
+
+  daysToSeconds(days) {
+    return (days * 86400);
+  }
+
+  monthsToSeconds(months) {
+    let daysInMonths = [31, 28, 31, 30, 31, 30, 30, 31, 30, 31, 30, 31];
+    let result = 0;
+    for (let i = 0; i < months; i++) {
+      result += this.daysToSeconds(daysInMonths[i]);
+    }
+
+    return result;
+  }
+
+  earthToMercury() {
+    return parseFloat((this.age * .24).toFixed(2));
+  }
+
+  earthToVenus() {
+    return parseFloat((this.age * .62).toFixed(2));
+  }
+
 }
--- spec/calc-spec.js
+++ spec/calc-spec.js
@@ -6,3 +6,24 @@ describe('yearsToSeconds', function () {
     expect(calc.yearsToSeconds()).toEqual(851472000);
   });
 });
+
+describe('yearsToSeconds', function () {
+  it('should return 432000', function () {
+    let calc = new Calc(27);
+    expect(calc.daysToSeconds(5)).toEqual(432000);
+  });
+});
+
+describe('monthsToSeconds', function () {
+  it('should return 432000', function () {
+    let calc = new Calc(27);
+    expect(calc.monthsToSeconds(5)).toEqual(13046400);
+  });
+});
+
+describe('earthToMercury', function () {
+  it('should return 6.48', function () {
+    let calc = new Calc(27);
+    expect(calc.earthToMercury()).toEqual(6.48);
+  });
+});
